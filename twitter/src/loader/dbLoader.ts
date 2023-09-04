4e93c8739676a4a64442ccdc658db7952b888f50
Asad Mehmood <asad.mehmood@techavenue.biz>
2023-09-04 14:32:08 +0500

    --- updated docker files ---

--- twitter/src/loader/dbLoader.ts
+++ twitter/src/loader/dbLoader.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { Sequelize } from 'sequelize-typescript'
 import { appConfig } from '../config/appConfig'
 
+console.log({"\n\n\n":appConfig.models})
 export const sequelize = new Sequelize({
     host: appConfig.host,
     port: appConfig.db_port,
